b3b736b57b24ffb51415bfdc26318912eca33b10
Juha <juha.raisala@gmail.com>
2016-01-27 11:09:14 +0200

    basic stuff

---
+++ app/routers.py
@@ -0,0 +1,15 @@
+from app import app
+
+@app.route('/')
+
+
+def index():
+    return 'Hello World!!'
+    address='Rautatienkatu'
+    return 'Hello World'
+
+print('This is not any more included in index() function')
+    
+    #this is comment#
+"""This is 
+comment"""
