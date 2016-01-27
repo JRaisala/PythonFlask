4ebc134507c46c3307460b6c9ba59c31148f559e
Juha <juha.raisala@gmail.com>
2016-01-27 14:39:56 +0200

    jotain

--- app/routers.py
+++ app/routers.py
@@ -1,15 +1,21 @@
 from app import app
+#RENDER_TEMPLAte gives you access to Jinja2 template engine
+from flask import render_template, request
 
 @app.route('/')
-
-
 def index():
-    return 'Hello World!!'
+    name = 'Markus'
     address='Rautatienkatu'
-    return 'Hello World'
+    return render_template('template_index.html', title=address, name=name)
 
-print('This is not any more included in index() function')
-    
-    #this is comment#
-"""This is 
-comment"""
+
+@app.route('/user/<name>')
+def user(name):
+    print(request.headers.get('User-Agent'))
+    return render_template('template_user.html',name=name)
+
+@app.route('/user',method=['GET','POST'])
+def userParams():
+    name = request.args.get('name')
+    return render_template('template_user.html', name=name)
+  
