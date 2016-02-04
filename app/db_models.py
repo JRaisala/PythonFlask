ff0467abc008fbb966443ef81b123a1089010978
Juha <juha.raisala@gmail.com>
2016-02-04 14:10:07 +0200

    jotain

--- app/db_models.py
+++ app/db_models.py
@@ -1,23 +1,32 @@
-from app import db
 
-class Users(db.Model);
-    id= db.Column(db.Integer,primary_key=True)
-    email = db.Column(db.String(128),unique=True)
-    passw = db.Column(db.String(128))
-    
-    def __init__(self,email,passw);
-        self.email = email
-        self.passw = passw
-        
-class Friends(db.Model);
-    id = db.Column(db.Integer, primary_key = true)
-    name = db.Column(db,String)
-    address = db.Column(db,String)
-    age = db.Column(db,Integer)
-    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
-    
-    def __init__(self,name,address,age,user_id);
-        self.name = name
-        self.address = adress
-        self.age = age
-        self.id = user_id
+from app import db
+from flask.ext.bcrypt import generate_password_hash
+
+class Users(db.Model):
+	id= db.Column(db.Integer,primary_key=True)
+	email = db.Column(db.String(128),unique=True)
+	passw = db.Column(db.String(128))
+	
+	friends = db.relationship('Friends',backref='user',lazy='dynamic')
+	
+	def __init__(self,email,passw):
+		self.email = email
+		self.passw = generate_password_hash(passw)
+	#def __str__(self):
+		#return self.email + ' ' + self.passw + ' ' + str(self.id) 
+		
+class Friends(db.Model):
+	id = db.Column(db.Integer, primary_key = True)
+	name = db.Column(db.String)
+	address = db.Column(db.String)
+	age = db.Column(db.Integer)
+	user_id= db.Column(db.Integer,db.ForeignKey('users.id'))
+	#This is Friends constructor
+	def __init__(self,name,address,age,user_id):
+		self.name = name
+		self.address = address
+		self.age = age
+		self.user_id = user_id
+	def __str__(self):
+		text = "This is friends object"
+		return text
--- app/routers.py
+++ app/routers.py
@@ -1,45 +1,92 @@
 from app import app
 #render_template gives you access to Jinja2 template engine
-from flask import render_template,request,make_response,flash
-from app.forms import LoginForm, Registerform
-from app.db.models import Users
-from app import app
+from flask import render_template,request,make_response,flash,redirect,session
+from app.forms import LoginForm,RegisterForm,FriendForm
+from app.db_models import Users,Friends
+from app import db
+from flask.ext.bcrypt import check_password_hash
+
+#for sql error handling 
+
 
 @app.route('/',methods=['GET','POST'])
 def index():
 	login = LoginForm()
 	#Check if get method
 	if request.method == 'GET':
-		return render_template('template_index.html',form=login)
+		return render_template('template_index.html',form=login,isLogged=False)
 	else:
 		#check if form data is valid
 		if login.validate_on_submit():
-			print(login.email.data)
-			print(login.passw.data)
-			return render_template('template_user.html')
+			#Check id correct useranme and password
+			user = Users.query.filter_by(email=login.email.data)
+			print(user)
+			if user.count(user.count() == 1) and (check_password_hash(user[0].passw,login.passw.data)):
+				print(user[0])
+				session['user_id'] = user[0].id
+				session['isLogged'] = True
+				#tapa 1
+				friends = Friends.query.filter_by(user_id=user[0].id)
+				print(friends)
+				return render_template('template_user.html',isLogged=True,friends=friends)
+			else:
+				flash('Wrong email or password')
+				return render_template('template_index.html',form=login,isLogged=False)
 		#form data was not valid
 		else:
 			flash('Give proper information to email and password fields!')
-			return render_template('template_index.html',form=login)
+			return render_template('template_index.html',form=login,isLogged=False)
 
 @app.route('/register',methods=['GET','POST'])
-def registerUser();
-    form = RegisterForm()
-    if request.method == 'GET';
-    return render_template('template_register.html' form=form)
+def registerUser():
+	form = RegisterForm()
+	if request.method == 'GET':
+		return render_template('template_register.html',form=form,isLogged=False)
+	else:
+		if form.validate_on_submit():
+			user = Users(form.email.data,form.passw.data)
+			try:
+				db.session.add(user)
+				db.session.commit()
+			except:
+				db.session.rollback()
+				flash('Username allready in use')
+				return render_template('template_register.html',form=form,isLogged=False)
+			flash("Name {0} registered.".format(form.email.data))
+			return redirect('/')
+		else:
+			flash('Invalid email address or no password given')
+			return render_template('template_register.html',form=form,isLogged=False)
+		
 
-    else:
-        if form.validate_on_submit();
-            user = Users(form.email.data,form.passw.data)
-            db.session.add(user)
-            db.session.commit()
-            flash("Name (0) registered".format(form.email.data))
-            return redirect('/')
-        else:
-            flash('Invalid Email address or no password given')
-             return render_template('template_register.html', form=form)
-        
-        
+@app.route('/friends',methods=['GET','POST'])
+def friends():
+	#Check that user has logged in before you let execute this route
+	if not('isLogged' in session) or (session['isLogged'] == False):
+		return redirect('/')
+	form = FriendForm()
+	if request.method == 'GET':
+		return render_template('template_friends.html',form=form,isLogged=True)
+	else:
+		if form.validate_on_submit():
+			temp = Friends(form.name.data,form.address.data,form.age.data,session['user_id'])
+			db.session.add(temp)
+			db.session.commit()
+			#tapa 2
+			user = Users.query.get(session['user_id'])
+			print(user.friends)
+			return render_template('template_user.html',isLogged=True,friends=user.friends)
+		else:
+			flash('Give proper values to all fields')
+			return render_template('template_friends.html',form=form,isLogged=True)
+	
+
+@app.route('/logout')
+def logout():
+	#delete user session (clear all values)
+	session.clear()
+	return redirect('/')
+	
 @app.route('/user/<name>')
 def user(name):
 	print(request.headers.get('User-Agent'))
