a1966edcd4f9beab4fb3251679995eacad7828f7
Juha <juha.raisala@gmail.com>
2016-02-09 08:58:15 +0200

    toiminnallisuutta

--- blueprint/auth/auth_blueprint.py
+++ blueprint/auth/auth_blueprint.py
@@ -1,13 +1,18 @@
-from flask import Blueprint,render_template,flash,request,redirect
+from flask import Blueprint,render_template,flash,request,redirect,session
 from app.forms import LoginForm,RegisterForm
 from app.db_models import Users,Friends
 from app import db
+from flask.ext.bcrypt import check_password_hash
 
 auth = Blueprint('auth',__name__,template_folder='templates')
 
+@auth.route('/index/<int:page>',methods=['GET','POST'])
 @auth.route('/',methods=['GET','POST'])
-def index():
+def index(page=1):
 	login = LoginForm()
+	if request.method == 'GET' and 'user_id' in session:
+		friends = Friends.query.filter_by(user_id=session['user_id']).paginate(page,10,False)
+		return render_template('template_user.html',isLogged=True,friends=friends)
 	#Check if get method
 	if request.method == 'GET':
 		return render_template('template_index.html',form=login,isLogged=False)
@@ -22,7 +27,7 @@ def index():
 				session['user_id'] = user[0].id
 				session['isLogged'] = True
 				#tapa 1
-				friends = Friends.query.filter_by(user_id=user[0].id)
+				friends = Friends.query.filter_by(user_id=user[0].id).paginate(page,10,False)
 				print(friends)
 				return render_template('template_user.html',isLogged=True,friends=friends)
 			else:
